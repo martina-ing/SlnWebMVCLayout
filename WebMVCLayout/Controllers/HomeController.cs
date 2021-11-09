77e83eb1a1746e1aed0c23113aba08912e94d0f2
Martina <m.lusardi@hotmail.com>
2021-11-09 12:47:22 -0300

    Add project files.

---
+++ WebMVCLayout/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fecha = DateTime.Now.ToShortDateString();
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Saludos = "Ejemplo en About";
+            return View();
+        
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
